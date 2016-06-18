dfcfdec5b8e296425c3c72b47435e8f5c42764be
Vadim Iskuchekov <vadim.deus@gmail.com>
2016-06-18 17:07:55 +1000

    Add models for test app

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
+    text = models.TextField()
+
+
+class News(models.Model):
+    pub_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class SillySearchTestCase(TestCase):
+    pass
